d3647e378b742ebcbe7a26e752c5953f8138470f
jannick <jannickboese@msn.com>
2013-11-07 07:49:37 +0100

    added add object action

---
+++ XmasProject/XmasEngineModel/Management/Actions/AddXmasObjectAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XmasEngineModel.Management.Actions
+{
+	public class AddXmasObjectAction : XmasAction
+	{
+		public XmasObject Object { get; private set; }
+
+		public AddXmasObjectAction(XmasObject xobj)
+		{
+			this.Object = xobj;
+		}
+
+		protected override void Execute()
+		{
+			XmasActor actor = Object as XmasActor;
+			if (actor != null)
+			{
+				actor.ActionManager = ActionManager;
+				actor.EventManager = EventManager;
+				actor.World = World;
+				actor.Factory = Factory;
+			}
+		}
+	}
+}
